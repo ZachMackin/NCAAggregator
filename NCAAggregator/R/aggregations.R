cfcdd8a70bae46490d2a1466322688b10a126ecd
Zach <zachmackin@tamu.edu>
2024-11-09 15:19:38 -0600

    creating some starters for the aggregation functions

---
+++ NCAAggregator/R/aggregations.R
@@ -0,0 +1,18 @@
+
+#Aggregation Functions
+
+trimmed_mean <- function(prediction_matrix, trim = 0.1) {
+  #[ToDo] Implement our trimmed mean function
+}
+
+bayesian_averaging <- function(prediction_matrix, priors = NULL){
+  #[ToDo] Implement Bayesian Averaging Function
+}
+
+exponential_smoothing <- function(prediction_matrix, alpha = 0.5){
+  #[ToDo] Implement exponential smoothing function
+}
+
+geometric_mean <- function(prediction_matrix) {
+  #[ToDo] Implement our geometric mean function
+}
