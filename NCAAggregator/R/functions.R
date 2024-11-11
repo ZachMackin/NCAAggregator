d7e194858ee48d82c70f4e80de84232f4b93cc1c
Zach <zachmackin@tamu.edu>
2024-11-10 22:12:33 -0600

    Implementing a skeleton for our models

---
+++ NCAAggregator/R/functions.R
@@ -0,0 +1,20 @@
+#some example models to utilize in the aggregator (potentially add in functionality to choose pretrained model weights vs having to train)
+
+#[ToDo] add some hyperparameters here
+nearest_neighbors <- function(data, ...) {
+  #[ToDo] Implement our nearest_neighbors funcion
+}
+
+log5_model <- function(data){
+  #[ToDo] Implement a log5 model
+}
+
+#[ToDo] add some hyperparameters here (i.e regularization term)
+linear_reg_model <- function(data){
+  #[ToDo] Implement the linear regression model
+}
+
+#[ToDo] add some hyperparameters here (i.e #layers, learning rate, optimizer, etc)
+simple_nueral_net <- function(data){
+  #[ToDo] Implement the DL model
+}
