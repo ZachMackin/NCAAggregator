3bbbaf0cb6c17e390541477fc8179ed5110b175e
Zach <zachmackin@tamu.edu>
2024-12-06 16:50:19 -0600

    Implementing our log5 model

--- R/functions.R
+++ R/functions.R
@@ -5,13 +5,42 @@ nearest_neighbors <- function(data, ...) {
   #[ToDo] Implement our nearest_neighbors funcion
 }
 
-log5_model <- function(data, pythogorean_param=11.5){
-  #[ToDo] Implement a log5 model
-  #Calulate Pythagorean Scores for each team
+log5_model <- function(data, pythagorean_param=11.5){
+  # Adjust Offensive and Defensive Efficiencies by 1.4% (home court advantage)
+  home_off_eff <- data$offensive_efficiency_home * 1.014
+  home_def_eff <- data$defensive_efficiency_home * 0.986
+  away_off_eff <- data$defensive_efficiency_away * 0.986
+  away_def_eff <- data$offensive_efficiency_away * 1.014
 
-  #use Log 5 to get win percentage
+  #Calculate Pythagorean Scores for Home and Away Teams
+  pythagorean_home <- (home_off_eff^pythagorean_param) /
+    ((home_off_eff^pythagorean_param) + (home_def_eff^pythagorean_param))
 
-  #Backtrack from normal CDF to get score prediction
+  pythagorean_away <- (away_off_eff^pythagorean_param) /
+    ((away_off_eff^pythagorean_param) + (away_def_eff^pythagorean_param))
+
+  #Calculate Log5 Probability for Home Team
+  win_prob_home <- (pythagorean_home - (pythagorean_home * pythagorean_away)) /
+    (pythagorean_home + pythagorean_away - (2 * pythagorean_home * pythagorean_away))
+
+  # Convert Win Probability to Margin
+  # Solve for Margin using the equation: Win Prob = 0.0271 * Margin + 0.4707 (from KP)
+  margin <- (win_prob_home - 0.4707) / 0.0271
+
+  #Calculate Average Score
+  # Average score is the mean of Offensive Efficiency * (Pace / 100) and Defensive Efficiency * (Pace / 100)
+  pace_home <- data$pace_home
+  pace_away <- data$pace_away
+  home_avg_score <- (home_off_eff * (pace_home / 100) + away_def_eff * (pace_away / 100)) / 2
+  away_avg_score <- (away_off_eff * (pace_away / 100) + home_def_eff * (pace_home / 100)) / 2
+  avg_score <- (home_avg_score + away_avg_score) / 2
+  print(avg_score)
+
+  # Calculate Home and Away Scores Using the Margin
+  home_score <- avg_score + (margin / 2)
+  away_score <- avg_score - (margin / 2)
+
+  return(list(home_score = home_score, away_score = away_score))
 }
 
 #[ToDo] add some hyperparameters here (i.e regularization term)
